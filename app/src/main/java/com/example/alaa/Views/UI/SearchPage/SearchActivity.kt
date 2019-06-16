d78dfa3b736df4d4571a1cf465c43eb946520cf6
Iman <imanghafoori1@gmail.com>
2019-06-16 19:35:53 +0430

    16June
        Search Page
    
        OnClick Listener Back Button

--- app/src/main/java/com/example/alaa/Views/UI/SearchPage/SearchActivity.kt
+++ app/src/main/java/com/example/alaa/Views/UI/SearchPage/SearchActivity.kt
@@ -2,16 +2,26 @@ package com.example.alaa.Views.UI.SearchPage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.alaa.R
-import com.example.alaa.Tools.Checkable_Card
+import com.example.alaa.Tools.CheckableCard
 
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity() , SearchbarComponent.backListener {
+
+
+    override fun onBackClicked() {
+        Toast.makeText(this , "please fucking show ... " , Toast.LENGTH_SHORT).show()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val card_pressed : Checkable_Card = findViewById(R.id.card_view_pressed)
+        val card_pressed : CheckableCard = findViewById(R.id.card_view_pressed)
 
+        val btn_back : SearchbarComponent  = findViewById(R.id.search_bar)
+
+        btn_back.setOnBackClickListener(this)
     }
 }
