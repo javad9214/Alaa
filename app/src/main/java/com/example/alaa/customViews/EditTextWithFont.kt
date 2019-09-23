ba7556ca4b3c0212c1dd50eaf185de20e3eaee58
javad9214 <javadhosseini14@gmail.com>
2019-09-23 19:17:30 +0330

    Create EditText With Font Custom View Create Info Form Custom view Fix some  issues Sep 23

---
+++ app/src/main/java/com/example/alaa/customViews/EditTextWithFont.kt
@@ -0,0 +1,29 @@
+package com.example.alaa.customViews
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import com.example.alaa.R
+import com.google.android.material.textfield.TextInputEditText
+
+class EditTextWithFont(context: Context, attrs: AttributeSet) : TextInputEditText(context, attrs) {
+
+    private val fontOrdinal: Int
+
+
+    init {
+
+        val typed: TypedArray = context.obtainStyledAttributes(attrs, R.styleable.EditTextWithFont, 0, 0)
+
+        fontOrdinal = typed.getInt(R.styleable.EditTextWithFont_EditTextFont, 2)
+
+        setEditTextTypeFace(context)
+
+        typed.recycle()
+    }
+
+    private fun setEditTextTypeFace(context: Context) {
+        val face = FontFace.getInstance().getFontFace(context, fontOrdinal)
+        this.typeface = face
+    }
+}
--- app/src/main/java/com/example/alaa/customViews/InfoForm.kt
+++ app/src/main/java/com/example/alaa/customViews/InfoForm.kt
@@ -4,31 +4,33 @@ import android.content.Context
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.FrameLayout
+import android.widget.LinearLayout
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.alaa.R
-import com.example.alaa.databinding.InfoCardBinding
+import com.example.alaa.databinding.InfoFormBinding
 
 /**
- * this custom view is for Showing Information of User such as Name , phone Number and  etc .
+ * this custom view is a form for getting User Information Data such as Name , phone Number and etc .
+ * and fields like EditText
  */
-class InfoCard(context: Context, attrs: AttributeSet? = null) : FrameLayout(context, attrs) {
 
-    private var title: String? = resources.getString(R.string.proPhone)
-    private var textValue: String? = resources.getString(R.string.demo_mobileNumber)
-    private var icon: Drawable? = ContextCompat.getDrawable(context, R.drawable.ic_phone_colored)
+class InfoForm(context: Context, attrs: AttributeSet? = null) : LinearLayout(context, attrs) {
+
+    private var title: String? = resources.getString(R.string.mobilePhoneNumber)
+    private var hint: String? = resources.getString(R.string.demo_mobileNumber)
+    private var icon: Drawable? = ContextCompat.getDrawable(context, R.drawable.ic_mobile_color)
 
     private lateinit var titleTextView: TextViewWithFont
-    private lateinit var valueTextView: TextViewWithFont
+    private lateinit var valueEditText: EditTextWithFont
     private lateinit var iconImageView: AppCompatImageView
 
-    private var binding: InfoCardBinding
+    private var binding: InfoFormBinding
 
     init {
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        binding = DataBindingUtil.inflate(inflater, R.layout.info_card, this, true)
+        binding = DataBindingUtil.inflate(inflater, R.layout.info_form, this, true)
 
         bindViews()
         loadAttributes(attrs)
@@ -37,25 +39,25 @@ class InfoCard(context: Context, attrs: AttributeSet? = null) : FrameLayout(cont
 
     private fun bindViews() {
         titleTextView = binding.title
-        valueTextView = binding.txValue
+        valueEditText = binding.edValue
         iconImageView = binding.icon
     }
 
     private fun loadAttributes(attrs: AttributeSet?) {
         // Load attributes
         val a = context.obtainStyledAttributes(
-                attrs, R.styleable.InfoCard, 0, 0)
+                attrs, R.styleable.InfoForm, 0, 0)
 
-        title = a.getString(R.styleable.InfoCard_title_infoCard)
-        textValue = a.getString(R.styleable.InfoCard_value_infoCard)
-        icon = a.getDrawable(R.styleable.InfoCard_icon_infoCard)
+        title = a.getString(R.styleable.InfoForm_title_infoForm)
+        hint = a.getString(R.styleable.InfoForm_hint_infoForm)
+        icon = a.getDrawable(R.styleable.InfoForm_icon_infoForm)
 
         a.recycle()
     }
 
     private fun setAttributes() {
         titleTextView.text = title
-        valueTextView.text = textValue
+        valueEditText.hint = hint
         iconImageView.setImageDrawable(icon)
     }
 
@@ -63,11 +65,12 @@ class InfoCard(context: Context, attrs: AttributeSet? = null) : FrameLayout(cont
         titleTextView.text = text
     }
 
-    fun setValue(text: String) {
-        valueTextView.text = text
-    }
-
     fun setIcon(icon: Drawable?) {
         iconImageView.setImageDrawable(icon)
     }
+
+    fun setHint(text: String) {
+        valueEditText.hint = text
+    }
+
 }
